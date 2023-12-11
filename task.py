130372824abb07889b8c72e71e61a4a7bcf7c066
Michał Wyszkowski <wyszkows@MacBook-Air-Micha.local>
2023-12-11 13:09:17 +0100

    first commit

---
+++ task.py
@@ -0,0 +1,45 @@
+from typing import List
+
+class Solution:
+    def findIndex(self, nums: List[int], target: int) -> int:
+        counter = 0 # counter for array index
+        position = [] # array index holder
+        for number in nums:
+            if number > target: #provided numbers are sorted, so if target is lower than number, there will be no occurences 
+                break
+            elif number == target:
+                position.append(counter)
+                counter = counter + 1
+            else:
+                counter = counter + 1
+        if not position:
+            position.append(-1)
+        return position
+        
+    def displayOutput(self, nums: List[int]):
+        if -1 in nums:
+            print("No element found")
+        else:
+            print("Element found at the: ")
+            for num in nums:
+                print(num+1, "(index", num,")")
+            if len(nums) == 1:
+                print("position")
+            else:
+                print("positions")
+            print(" ")
+
+
+x = Solution()
+
+nums1 = [2, 3, 5, 7, 9]
+target1 = 7
+x.displayOutput(x.findIndex(nums1, target1))
+
+nums2 = [1, 2, 3, 4, 4, 5, 6, 7]
+target2 = 4
+x.displayOutput(x.findIndex(nums2, target2))
+
+nums3 = [1, 4, 5, 8, 9]
+target3 = 2
+x.displayOutput(x.findIndex(nums3, target3))
